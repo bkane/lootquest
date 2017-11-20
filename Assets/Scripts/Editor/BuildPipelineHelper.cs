899d4b33e75d974bfc289526db7c890cce1da870
bkane <benjaminkane@gmail.com>
2017-11-20 11:55:38 -0500

    Add initial build pipeline script.

---
+++ Assets/Scripts/Editor/BuildPipelineHelper.cs
@@ -0,0 +1,28 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Assets.Scripts.Editor
+{
+    public class BuildPipelineHelper
+    {
+        protected static string BUILD_FOLDER = "Builds/LootBoxQuest"; //will be cleaned
+
+        [MenuItem("Build/Build for Submission")]
+        public static void RunBuilds()
+        {
+            string buildType = "Build for Submission";
+
+            Debug.LogFormat("{0}: started {1}", buildType, System.DateTime.Now.ToLocalTime());
+
+            FileUtil.DeleteFileOrDirectory(BUILD_FOLDER);
+
+            var levels = new[] { "Assets/Scenes/main.unity" };
+
+            BuildPipeline.BuildPlayer(levels, BUILD_FOLDER + "/lootquest.exe", BuildTarget.StandaloneWindows, BuildOptions.None);
+
+            //TODO: zip, etc
+
+            Debug.LogFormat("{0}: completed {1}", buildType, System.DateTime.Now.ToLocalTime());
+        }
+    }
+}
