d40328328090ecdf7c8af350f08457186556be0e
bkane <benjaminkane@gmail.com>
2017-11-25 18:51:19 -0500

    more ui progress for macguffin quest

--- Assets/Scripts/ViewControllers/MacGuffinQuestViewController.cs
+++ Assets/Scripts/ViewControllers/MacGuffinQuestViewController.cs
@@ -6,13 +6,18 @@ using UnityEngine.UI;
 namespace Assets.Scripts.ViewControllers
 {
     public class MacGuffinQuestViewController : MonoBehaviour
-    { 
+    {
         //Labels
-        public TextMeshProUGUI GrindProgressText;
-        public TextMeshProUGUI MacGuffinsUnlockedText;
+        public Slider GrindProgress;
+        public TextMeshProUGUI GrindButtonText;
+        //public TextMeshProUGUI MacGuffinsUnlockedText;
         public TextMeshProUGUI LootBoxText;
         public TextMeshProUGUI ItemsText;
         public TextMeshProUGUI BotAccountsText;
+
+        public GameObject LootBoxPanel;
+        public GameObject SellItemsPanel;
+        public GameObject BotsPanel;
 
         //Buttons
         public Button GrindButton;
@@ -33,29 +38,33 @@ namespace Assets.Scripts.ViewControllers
 
         private void Update()
         {
-            GrindProgressText.text = string.Format("Grind: {0}%", LootBoxModel.Instance.GrindProgress);
+            GrindProgress.value = LootBoxModel.Instance.GrindProgress / 100f;
+
             LootBoxText.text = string.Format("Loot Boxes: {0}", LootBoxModel.Instance.LootBoxes);
-            ItemsText.text = string.Format("Emotes and Skins: {0}", LootBoxModel.Instance.TrashItems);
-            BotAccountsText.text = string.Format("Bot Accounts: {0}", LootBoxModel.Instance.NumBotAccounts);
-            MacGuffinsUnlockedText.text = string.Format("MacGuffins Unlocked: {0}", LootBoxModel.Instance.MacGuffinUnlocked);
+            ItemsText.text = string.Format("{0}", LootBoxModel.Instance.TrashItems);
+            BotAccountsText.text = string.Format("{0}", LootBoxModel.Instance.NumBotAccounts);
+            //MacGuffinsUnlockedText.text = string.Format("MacGuffins Unlocked: {0}", LootBoxModel.Instance.MacGuffinUnlocked);
 
             BuyBotAccountButton.gameObject.SetActive(LootBoxModel.Instance.UpgradeManager.IsActive(Upgrade.EUpgradeType.AutoGrinder) && !LootBoxModel.Instance.MacGuffinQuest.HideBotButton);
             BotAccountsText.gameObject.SetActive(LootBoxModel.Instance.UpgradeManager.IsActive(Upgrade.EUpgradeType.AutoGrinder));
-            BuyBotAccountButton.GetComponentInChildren<Text>().text = string.Format("Buy Bot Account (${0})", LootBoxModel.Instance.MacGuffinQuest.CostPerBot());
+            BuyBotAccountButton.GetComponentInChildren<TextMeshProUGUI>().text = string.Format("Buy Bot Account (${0})", LootBoxModel.Instance.MacGuffinQuest.CostPerBot());
 
             bool showLoot = LootBoxModel.Instance.TotalLootBoxes > 0;
-            LootBoxText.gameObject.SetActive(showLoot);
-            OpenLootBoxButton.gameObject.SetActive(showLoot);
-
+            LootBoxPanel.SetActive(showLoot);
+            
             bool showTrash = LootBoxModel.Instance.LootBoxesOpened > 0;
-            ItemsText.gameObject.SetActive(showTrash);
-            SellTrashItemButton.gameObject.SetActive(showTrash);
+            SellItemsPanel.SetActive(showLoot);
+
+            bool showBots = LootBoxModel.Instance.NumBotAccounts > 0;
+            BotsPanel.SetActive(showBots);
 
             int actions = (int) LootBoxModel.Instance.MacGuffinQuest.ActionsPerClick();
             string actionsString = actions == 1 ? string.Empty : string.Format("(x{0})", actions);
-            GrindButton.GetComponentInChildren<Text>().text = string.Format("{0} {1}", LootBoxModel.Instance.GrindsCompleted > 5 ? "Grind" : "Play", actionsString);
-            OpenLootBoxButton.GetComponentInChildren<Text>().text = string.Format("Open Loot Box {0}", actionsString);
-            SellTrashItemButton.GetComponentInChildren<Text>().text = string.Format("Sell Trash Items on Auction House {0}", actionsString);
+
+            //TODO: have an indicator of actions count
+            //GrindButtonText.text = string.Format("{0} {1}", LootBoxModel.Instance.GrindsCompleted > 5 ? "Grind" : "Play", actionsString);
+            //OpenLootBoxButton.GetComponentInChildren<TextMeshProUGUI>().text = string.Format("Open Loot Box {0}", actionsString);
+            //SellTrashItemButton.GetComponentInChildren<TextMeshProUGUI>().text = string.Format("Sell Trash Items {0}", actionsString);
         }
     }
 }
