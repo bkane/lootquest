cc949d1a845bd8f7bae65df768b5343763da4fa9
bkane <benjaminkane@gmail.com>
2017-11-29 19:39:20 -0500

    display and copy font licenses properly

--- Assets/Scripts/Editor/BuildPipelineHelper.cs
+++ Assets/Scripts/Editor/BuildPipelineHelper.cs
@@ -1,4 +1,5 @@
-﻿using UnityEditor;
+﻿using System.IO;
+using UnityEditor;
 using UnityEngine;
 
 namespace Assets.Scripts.Editor
@@ -21,6 +22,13 @@ namespace Assets.Scripts.Editor
             BuildPipeline.BuildPlayer(levels, BUILD_FOLDER + "/lootquest.exe", BuildTarget.StandaloneWindows, BuildOptions.None);
 
             //TODO: zip, etc
+            Directory.CreateDirectory(BUILD_FOLDER + "/licenses");
+
+            foreach (var file in Directory.GetFiles("Assets/licenses", "*.txt"))
+            {
+                string filename = Path.GetFileName(file); ;
+                FileUtil.CopyFileOrDirectory("Assets/licenses/" + filename, BUILD_FOLDER + "/licenses/" + filename);
+            }
 
             Debug.LogFormat("{0}: completed {1}", buildType, System.DateTime.Now.ToLocalTime());
         }
